787a108ee6fe0d5c907eca35397acfea52a289cc
Sean <sean.stofberg@connect.qut.edu.au>
2020-03-06 16:03:41 +1000

    Added backend functionality for handling POST, PUT and DELETE requests

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+app.use(cors());
+app.options('*', cors());
+/* Previous cors code before npm install cors --save code ^^
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-//const sql = require('mssql');
+*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express api' });
 });
 
-/* GET Price_AIRCoop Data */
+/* READ Price_AIRCoop Data */
 router.get('/aircoop', (req, res, next) => {
     // connect to your database
     sql.connect(dbConfig, (err) => {
@@ -38,7 +38,7 @@ router.get('/aircoop', (req, res, next) => {
         var request = new sql.Request();
            
         // query to the database and get the records
-        request.query('select * from Price_AIRCoop', (err, result) => {
+        request.query('SELECT * FROM Price_AIRCoop', (err, result) => {
             
             if (err) console.log(err)
 
@@ -49,11 +49,11 @@ router.get('/aircoop', (req, res, next) => {
     });
 });
 
-/* GET Price_AIRCoop Data */
+/* CREATE Price_AIRCoop Data */
 router.post('/aircoop', (req, res) => {
   var data = req.body;
   console.log(data)
-  
+
   // connect to your database
   sql.connect(dbConfig, (err) => {
 
@@ -62,10 +62,9 @@ router.post('/aircoop', (req, res) => {
       // create Request object
       var request = new sql.Request();
 
-      var sqlQuery = 'INSERT INTO Price_AIRCoop (SiteID, Carrier, Route, CampaignName, CampaignMargin, CampaignFixedMargin, CampaignStartDate, CampaignEndDate, DepartAfter, DepartBefore, Origin, Destination, COIPercentage, MaximumCOIPercentage, FixedCostPercentage) VALUES (' + "'" + data.SiteID + "'" + ',' + "'" + data.Carrier + "'" + ',' + "'" + data.Route + "'" + ',' + "'" + data.CampaignName + "'" + ',' + "'" + data.CampaignMargin + "'" + ',' + "'" + data.CampaignFixedMargin + "'" + ',' + "'" + data.CampaignStartDate + "'" + ',' + "'" + data.CampaignEndDate + "'" + ',' + "'" + data.DepartAfter + "'" + ',' + "'" + data.DepartBefore + "'" + ',' + "'" + data.Origin + "'" + ',' + "'" + data.Destination + "'" + ',' + "'" + data.COIPercentage + "'" + ',' + "'" + data.MaximumCOIPercentage + "'" + ',' + "'" + data.FixedCostPercentage + "'" + ')';
-      // , CampaignFixedMargin, CampaignStartDate, CampaignEndDate, DepartAfter, DepartBefore, Origin, Destination, COIPercentage, MaximumCOIPercentage, FixedCostPercentage
-      console.log(sqlQuery);
-      // query to the database and get the records
+      var sqlQuery = `INSERT INTO Price_AIRCoop (SiteID, Carrier, Route, CampaignName, CampaignMargin, CampaignFixedMargin, CampaignStartDate, CampaignEndDate, DepartAfter, DepartBefore, Origin, Destination, COIPercentage, MaximumCOIPercentage, FixedCostPercentage) VALUES ('${data.SiteID}', '${data.Carrier}', '${data.Route}', '${data.CampaignName}', '${data.CampaignMargin}', '${data.CampaignFixedMargin}', '${data.CampaignStartDate}', '${data.CampaignEndDate}', '${data.DepartAfter}', '${data.DepartBefore}', '${data.Origin}', '${data.Destination}', '${data.COIPercentage}', '${data.MaximumCOIPercentage}', '${data.FixedCostPercentage}')`;
+
+      // query to the database and submit the new record
       request.query(sqlQuery, (err, result) => {
 
           if (err) console.log(err)
@@ -75,6 +74,63 @@ router.post('/aircoop', (req, res) => {
 
       });
   });
+});
+
+/* UPDATE Price_AIRCoop Data */
+router.put('/aircoop/:id', (req, res) => {
+  var data = req.body;
+
+  // connect to your database
+  sql.connect(dbConfig, (err) => {
+
+      if (err) console.log(err);
+
+      // create Request object
+      var request = new sql.Request();
+
+      var sqlQuery = `UPDATE Price_AIRCoop SET SiteID = '${data.SiteID}', Carrier = '${data.Carrier}', Route = '${data.Route}', CampaignName = '${data.CampaignName}', CampaignMargin = '${data.CampaignMargin}', CampaignFixedMargin = '${data.CampaignFixedMargin}', CampaignStartDate = '${data.CampaignStartDate}', CampaignEndDate = '${data.CampaignEndDate}', DepartAfter = '${data.DepartAfter}', DepartBefore = '${data.DepartBefore}', Origin = '${data.Origin}', Destination = '${data.Destination}', COIPercentage = '${data.COIPercentage}', MaximumCOIPercentage = '${data.MaximumCOIPercentage}', FixedCostPercentage = '${data.FixedCostPercentage}' WHERE ID = ${req.params.id}`;
+
+      // query to the database and set the record
+      request.query(sqlQuery, (err, result) => {
+
+          if (err) console.log(err)
+
+          // send records as a response
+          res.send(JSON.stringify(result, null, 4));
+
+      });
+  });
+  
+});
+
+/* DELETE Price_AIRCoop Data */
+router.delete('/aircoop/:id', (req, res) => {
+  var data = req.body;
+  
+  console.log(req.params.id);
+
+  // connect to your database
+  sql.connect(dbConfig, (err) => {
+
+      if (err) console.log(err);
+
+      // create Request object
+      var request = new sql.Request();
+
+      // create query to handle single and mutliple deletes
+      var sqlQuery = `DELETE FROM Price_AIRCoop WHERE ID IN (${req.params.id})`;
+      console.log(sqlQuery);
+      // query to the database and delete the record(s)
+      request.query(sqlQuery, (err, result) => {
+
+          if (err) console.log(err)
+
+          // send records as a response
+          res.send(JSON.stringify(result, null, 4));
+
+      });
+  });
+  
 });
 
 module.exports = router;
