549c8191b9f290a13f9a6eefbac3610988aa0329
Sean <sean.stofberg@connect.qut.edu.au>
2020-06-04 15:14:11 +1000

    Changed FrontEndCommission route handling to use async methods

--- routes/frontEndCommission.js
+++ routes/frontEndCommission.js
@@ -1,128 +1,99 @@
 const express = require('express');
-const sql = require('mssql');
+const { poolPromise } = require('../db.js');
 const router = express.Router();
 
-var dbConfig = {
-  server: 'localhost',
-  port: 1433,
-  user: 'SA',
-  password: 'Password01',
-  database: 'Capstone_DB',
-  connectionTimeout: 150000,
-  driver: 'tedious',
-  stream: false,
-  options: {
-    appName: 'Server',
-    encrypt: false
-  },
-  pool: {
-    max: 20,
-    min: 0,
-    idleTimeoutMillis: 30000
-  }
-}
-
 /* READ Price_FrontEndCommission Data */
-router.get('/', (req, res, next) => {
-    // connect to your database
-    sql.connect(dbConfig, (err) => {
-    
-        if (err) console.log(err);
+router.get('/', async (req, res, next) => {
+  try {
 
-        // create Request object
-        var request = new sql.Request();
-           
-        // query to the database and get the records
-        request.query('SELECT * FROM Price_FrontEndCommission', (err, result) => {
-            
-            if (err) console.log(err)
+    // Create request with mssql database server pool
+    const pool = await poolPromise; 
+    const request = pool.request();
 
-            // send records as a response
-            res.send(JSON.stringify(result.recordset, null, 4));
-            
-        });
-    });
+    // Construct mssql query
+    var sqlQuery = 'SELECT * FROM Price_FrontEndCommission';
+
+    // Wait for response then send to frontend
+    const response = await request.query(sqlQuery);
+    res.status(200).json(response.recordset);
+
+  } catch (err) {
+
+    console.log(err);
+    next(err);        // Use express async next function to handle errors
+
+  }
 });
 
 /* CREATE Price_FrontEndCommission Data */
-router.post('/', (req, res) => {
-  var data = req.body;
-  console.log(data)
+router.post('/', async (req, res, next) => {
+  try {
 
-  // connect to your database
-  sql.connect(dbConfig, (err) => {
+    // Create request with mssql database server pool
+    const pool = await poolPromise; 
+    const request = pool.request();
 
-      if (err) console.log(err);
+    // Construct mssql query
+    var data = req.body;
+    var sqlQuery = `INSERT INTO Price_FrontEndCommission (Source, SiteID, Carrier, TripType, Class, FrontEndCommission, Route, BookingDate, BookingEndDate, DepartureStartDate, DepartureEndDate) VALUES ('${data.Source}', '${data.SiteID}', '${data.Carrier}', '${data.TripType}', '${data.Class}', '${data.FrontEndCommission}', '${data.Route}', '${data.BookingDate}', '${data.BookingEndDate}', '${data.DepartureStartDate}', '${data.DepartureEndDate}')`;
 
-      // create Request object
-      var request = new sql.Request();
+    // Wait for response then send to frontend
+    const response = await request.query(sqlQuery);
+    res.status(200).json(response.recordset);
 
-      var sqlQuery = `INSERT INTO Price_FrontEndCommission (Source, SiteID, Carrier, TripType, Class, FrontEndCommission, Route, BookingDate, BookingEndDate, DepartureStartDate, DepartureEndDate) VALUES ('${data.Source}', '${data.SiteID}', '${data.Carrier}', '${data.TripType}', '${data.Class}', '${data.FrontEndCommission}', '${data.Route}', '${data.BookingDate}', '${data.BookingEndDate}', '${data.DepartureStartDate}', '${data.DepartureEndDate}')`;
+  } catch (err) {
 
-      // query to the database and submit the new record
-      request.query(sqlQuery, (err, result) => {
+    console.log(err);
+    next(err);        // Use express async next function to handle errors
 
-          if (err) console.log(err)
-
-          // send records as a response
-          res.send(JSON.stringify(result, null, 4));
-
-      });
-  });
+  }
 });
 
 /* UPDATE Price_FrontEndCommission Data */
-router.put('/:id', (req, res) => {
-  var data = req.body;
+router.put('/:id', async (req, res, next) => {
+  try {
 
-  // connect to your database
-  sql.connect(dbConfig, (err) => {
+    // Create request with mssql database server pool
+    const pool = await poolPromise; 
+    const request = pool.request();
 
-      if (err) console.log(err);
+    // Construct mssql query
+    var data = req.body;
+    var sqlQuery = `UPDATE Price_FrontEndCommission SET Source = '${data.Source}', SiteID = '${data.SiteID}', Carrier = '${data.Carrier}', TripType = '${data.TripType}', Class = '${data.Class}', FrontEndCommission = '${data.FrontEndCommission}', Route = '${data.Route}', BookingDate = '${data.BookingDate}', BookingEndDate = '${data.BookingEndDate}', DepartureStartDate = '${data.DepartureStartDate}', DepartureEndDate = '${data.DepartureEndDate}' WHERE ID = ${req.params.id}`;
 
-      // create Request object
-      var request = new sql.Request();
+    // Wait for response then send to frontend
+    const response = await request.query(sqlQuery);
+    res.status(200).json(response.recordset);
 
-      var sqlQuery = `UPDATE Price_FrontEndCommission SET Source = '${data.Source}', SiteID = '${data.SiteID}', Carrier = '${data.Carrier}', TripType = '${data.TripType}', Class = '${data.Class}', FrontEndCommission = '${data.FrontEndCommission}', Route = '${data.Route}', BookingDate = '${data.BookingDate}', BookingEndDate = '${data.BookingEndDate}', DepartureStartDate = '${data.DepartureStartDate}', DepartureEndDate = '${data.DepartureEndDate}' WHERE ID = ${req.params.id}`;
+  } catch (err) {
 
-      // query to the database and set the record
-      request.query(sqlQuery, (err, result) => {
+    console.log(err);
+    next(err);        // Use express async next function to handle errors
 
-          if (err) console.log(err)
-
-          // send records as a response
-          res.send(JSON.stringify(result, null, 4));
-
-      });
-  });
-  
+  }
 });
 
 /* DELETE Price_FrontEndCommission Data */
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res, next) => {
+  try {
 
-  // connect to your database
-  sql.connect(dbConfig, (err) => {
+    // Create request with mssql database server pool
+    const pool = await poolPromise; 
+    const request = pool.request();
 
-      if (err) console.log(err);
+    // Construct mssql query
+    var sqlQuery = `DELETE FROM Price_FrontEndCommission WHERE ID IN (${req.params.id})`;
 
-      // create Request object
-      var request = new sql.Request();
+    // Wait for response then send to frontend
+    const response = await request.query(sqlQuery);
+    res.status(200).json(response.recordset);
 
-      // create query to handle single and mutliple deletes
-      var sqlQuery = `DELETE FROM Price_FrontEndCommission WHERE ID IN (${req.params.id})`;
-      console.log(sqlQuery);
-      // query to the database and delete the record(s)
-      request.query(sqlQuery, (err, result) => {
+  } catch (err) {
 
-          if (err) console.log(err)
+    console.log(err);
+    next(err);        // Use express async next function to handle errors
 
-          // send records as a response
-          res.send(JSON.stringify(result, null, 4));
-
-      });
-  });
-  
+  }
 });
 
 module.exports = router;
